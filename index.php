5f8c4e96243ee44113c28d28e47fc6b8cb4080fb
Hiroki Kato <hirokikato1@gmail.com>
2024-02-15 15:24:06 -0500

    Revert "created Application and Router classes"
    This reverts commit 1e767693afcec85175ad3d413187339ad7ea87ac.

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
 
 //run server php -S localhost:port
-require_once 'Application.php';
 
-
-$app = new Application();
-
-// $router = new Router();
-// $app->userRouter($router);
-
-$app->router->get('/', function () {
-    return 'Hello world';
-});
-
-
-
-$app->run();
+echo "Hello world";
