82738526ecbf59e39e964bcdccf1da7bc89d9644
Hiroki Kato <hirokikato1@gmail.com>
2024-04-09 15:18:36 -0400

    moved index.php to public folder

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+//run server php -S localhost:port
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use app\core\Application;
+
+$app = new Application();
+
+
+$app->router->get('/', function () {
+    return 'Hello world';
+});
+
+$app->router->get('/contact', function () {
+    return 'Contact page';
+});
+
+$app->router->get('/test', function () {
+    return 'the test works';
+});
+
+$app->run();
