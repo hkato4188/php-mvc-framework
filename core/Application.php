2fe8c869e5750240cce918d2bebf97290851760b
Hiroki Kato <hirokikato1@gmail.com>
2024-05-17 14:43:35 -0400

    corrected bug in Application constructor--Router requeires request and response objects upon initialization

--- core/Application.php
+++ core/Application.php
@@ -16,8 +16,8 @@ class Application
         self::$ROOT_DIR = $rootPath;
         self::$app = $this;
         $this->request = new Request();
-        $this->router = new Router($this->request);
         $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
 
     }
 
